cd9064f51e92b173a4c9d2b84da5eafef9813962
ozitronics <83677651+ozitronics@users.noreply.github.com>
2021-05-08 11:13:47 +1000

    Add files via upload

---
+++ Examples/LoRa1284_LED_brightness_receiver/LoRa1284_LED_brightness_receiver.ino
@@ -0,0 +1,72 @@
+/******************************************************************************
+ LoRa1284 demo code - Receiver
+ Requires a LoRa module and antenna fitted to the LoRa1284 board
+ 
+ Receive data value via LoRa and use it to vary onboard led brightness.
+ 
+ Each received value is also sent via serial port and can be viewed using
+ the Arduino IDE serial monitor (9600 baud).
+ *****************************************************************************/
+
+/*****************************************************************************/
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+
+/*****************************************************************************/
+// LoRa library
+#include <LoRa.h>
+
+const int LORA_CS   = 4;          // LoRa chip select (SS)
+const int LORA_RST  = 0;          // LoRa reset
+const int LORA_IRQ  = 10;         // LoRa interrupt input
+
+/*****************************************************************************/
+// Global variables
+int val = 0;                      // default = 1/2 brightness
+
+/******************************************************************************
+ I/O pin definitions and program defines                                     */
+const int led = 12;               // led
+
+/*****************************************************************************/
+void setup()
+{
+  /** configure I/O pins */
+  pinMode(led,      OUTPUT);
+  pinMode(LORA_CS,  OUTPUT);
+  pinMode(LORA_RST, OUTPUT);
+  pinMode(LORA_IRQ, INPUT);
+
+  Serial.begin(9600);
+  while (!Serial);
+
+  /****************************************************************************
+  setup and start LoRa module
+  if it doesn't start print error message
+  ****************************************************************************/
+  LoRa.setPins(LORA_CS, LORA_RST, LORA_IRQ);    // define LoRa1284 pin usage
+
+  Serial.println("LoRa Receiver");
+  if (!LoRa.begin(433E6)) {                     // 433MHz
+                                                // for 868MHZ use "868E6"
+                                                // for 915MHz use "915E6"
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+/*****************************************************************************/
+
+/*****************************************************************************/
+void loop()
+{
+  // try to parse packet
+  if (LoRa.parsePacket()) {     // if received a packet...
+    if (LoRa.available()) {     //  and if data available...
+      val = LoRa.parseInt();    // read data variable from received packet
+      Serial.println(val);      // print it
+      analogWrite(led, val);    // and adjust led brightness
+    }
+  }
+}
+/*****************************************************************************/
